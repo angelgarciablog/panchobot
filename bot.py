ef9b04ed3dce8ec45fd81b187b0da72c9194c744
Glitch (superbottele) <none>
2020-04-04 23:46:10 +0000

    💡🎈 Checkpoint ./bot.py:13712728/528

--- bot.py
+++ bot.py
@@ -1,23 +1,10 @@
 import telebot
+import random
 from os import environ
 
 bot = telebot.TeleBot(environ['TELEGRAM_TOKEN'])
 
-bot_text = '''
-Howdy, how are you doing?
-Source code on https://glitch.com/~{}
-'''.format(environ['PROJECT_NAME'])
-
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, bot_text)
-
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-  cid = message.chat.id
-  if message.text.lower() == "hola":
-    bot.send_message( cid, 'hola amigo')
 
 
-bot.set_webhook("https://{}.glitch.me/{}".format(environ['PROJECT_NAME'], environ['TELEGRAM_TOKEN']))
+
 
