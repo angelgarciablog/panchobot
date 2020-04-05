6f61148bd57633a3f19513eba211d3ae0dbcb050
Glitch (superbottele) <none>
2020-04-05 00:22:09 +0000

    ✂️🎉 Checkpoint ./bot.py:13712728/662

--- bot.py
+++ bot.py
@@ -18,15 +18,16 @@ def echo_message(message):
   if message.text.lower() == "hola" or "hla" or "hl":
     bot.send_message( cid, random.choice(holas))
   
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+  cid = message.chat.id
+  buen_dia="buen dia que la pases bien","buen dia","buen dia precioso(a)","que dios bendiga tu dia"
+  if message.text.lower() == "buenos dias" or "buen dia":
+    bot.send_message(cid, random.choice(buen dia)
+                     
+  
 @bot.message_handler(commands=["reglas"])
 def reglas(message):
-  bot.reply_to(message, "❗❗❗ *REGLAS* ❗❗❗\n
-✅ Enlaces no spam\n
-✅ Archivos sin virus\n
-❗❗Respetar a los miembros del grupo❗❗\n
-No porno, cp, gore, y similares \n
-🚫 Virus\n
-🚫 Cadenas con información falsa\n
-🚫 Molestar al privado")
+  bot.reply_to(message, "REGLAS\n no spam\n no flow\n no virus \n respetar a los miembros del grupo,\n No molestar al privado a ningun miembro de este grupo")
 
 bot.set_webhook("https://{}.glitch.me/{}".format(environ['PROJECT_NAME'], environ['TELEGRAM_TOKEN']))
